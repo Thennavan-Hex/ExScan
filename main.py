b680ab24c178be5b995cc1c0b7381650e61752d0
Thennavan-Hex <thensthennavan3@gmail.com>
2023-12-26 18:34:25 +0530

    updated

--- main.py
+++ main.py
@@ -1,64 +1,69 @@
-[200~import nmap
-  import socket
-  from colorama import init, Fore, Style
+import nmap
+import socket
+from colorama import init, Fore, Style
+import pyfiglet
 
-  init(autoreset=True)
+banner = pyfiglet.figlet_format("ExScan", font="standard")
+print(banner)
 
-  def reverse_dns_lookup(ip_address):
-      try:
-          domain_name = socket.gethostbyaddr(ip_address)
-          return domain_name[0]
-      except socket.herror:
-          return "Reverse DNS lookup failed"
+init(autoreset=True)
 
+def reverse_dns_lookup(ip_address):
+    try:
+        domain_name = socket.gethostbyaddr(ip_address)
+        return domain_name[0]
+    except socket.herror:
+        return "Reverse DNS lookup failed"
 
-  def detect_os(target_ip):
-      nm = nmap.PortScanner()
-      nm.scan(target_ip, arguments='-O')
+def detect_os(target_ip):
+    nm = nmap.PortScanner()
+    nm.scan(target_ip, arguments='-O')
 
-      if 'osclass' in nm[target_ip]:
-          os_details = nm[target_ip]['osclass'][0]
-          return f"{Fore.GREEN}OS Details: {os_details['osfamily']} {os_details['osgen']} ({os_details['accuracy']}%)"
-      else:
-          return f"{Fore.RED}OS Details not found"
+    if 'osclass' in nm[target_ip]:
+        os_details = nm[target_ip]['osclass'][0]
+        return f"{Fore.GREEN}OS Details: {os_details['osfamily']} {os_details['osgen']} ({os_details['accuracy']}%)"
+    else:
+        return f"{Fore.RED}OS Details not found"
 
+def scan(target):
+    nm = nmap.PortScanner()
+    nm.scan(target, arguments='-sV')
 
-  def scan(target):
-      nm = nmap.PortScanner()
-      nm.scan(target, arguments='-sV')
+    for host in nm.all_hosts():
+        print(f"{Fore.BLUE}Nmap scan report for {host}")
+        print(f"{Fore.YELLOW}Host is up ({nm[host].hostname()})")
+        print(f"{Style.RESET_ALL}-----------")
+        print(f"{Fore.CYAN}Protocol: tcp")
+        print(f"PORT     STATE     SERVICE     VERSION")
 
-      for host in nm.all_hosts():
-          print(f"{Fore.BLUE}Nmap scan report for {host}")
-          print(f"{Fore.YELLOW}Host is up ({nm[host].hostname()})")
-          print(f"{Style.RESET_ALL}-----------")
-          print(f"{Fore.CYAN}Protocol: tcp")
-          print(f"PORT     STATE     SERVICE     VERSION")
+        ports = nm[host]['tcp'].keys()
+        sorted_ports = sorted(ports)
 
-          ports = nm[host]['tcp'].keys()
-          sorted_ports = sorted(ports)
+        for port in sorted_ports:
+            state = nm[host]['tcp'][port]['state']
+            service = nm[host]['tcp'][port]['name']
+            version = nm[host]['tcp'][port].get('product', 'Unknown Version')
 
-          for port in sorted_ports:
-              state = nm[host]['tcp'][port]['state']
-              service = nm[host]['tcp'][port]['name']
-              version = nm[host]['tcp'][port].get('product', 'Unknown Version')
+            print(f"{port}/tcp   {state}      {service}     {version}")
 
-              print(f"{port}/tcp   {state}      {service}     {version}")
+if __name__ == "__main__":
+    try:
+        target = input("Enter target to scan (e.g., example.com): ")
 
-  if __name__ == "__main__":
-      target = input("Enter target to scan (e.g., thennavan.tech): ")
-
-      ip_addresses = socket.getaddrinfo(target, None)
-      ipv4_address = [addr[4][0] for addr in ip_addresses if addr[0] == socket.AF_INET][0]
-    ipv6_address = [addr[4][0] for addr in ip_addresses if addr[0] == socket.AF_INET6]
+        ip_addresses = socket.getaddrinfo(target, None)
+        ipv4_address = [addr[4][0] for addr in ip_addresses if addr[0] == socket.AF_INET][0]
+        ipv6_address = [addr[4][0] for addr in ip_addresses if addr[0] == socket.AF_INET6]
 
         print(f"\n{Fore.MAGENTA}IPv4 Address for {target}: {ipv4_address}")
-            if ipv6_address:
-                        print(f"IPv6 Addresses for {target}: {', '.join(ipv6_address)}")
+        if ipv6_address:
+            print(f"IPv6 Addresses for {target}: {', '.join(ipv6_address)}")
 
-                            scan(target)
+        scan(target)
 
-                                domain_name = reverse_dns_lookup(ipv4_address)
-                                    print(f"\n{Fore.GREEN}The associated domain for IP {ipv4_address} is: {domain_name}")
+        domain_name = reverse_dns_lookup(ipv4_address)
+        print(f"\n{Fore.GREEN}The associated domain for IP {ipv4_address} is: {domain_name}")
 
-                                        print(detect_os(ipv4_address))
+        print(detect_os(ipv4_address))
 
+    except KeyboardInterrupt:
+        print(f"\n{Fore.RED}User interrupted the scanning process. Exiting...")
